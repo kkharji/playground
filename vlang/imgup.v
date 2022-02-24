6f0e621eab82f73a97835ea35d71a1904861c6c6
tami5 <kkharji@protonmail.com>
2022-02-24 07:14:43 +0300

    script(imgup): support macos

--- vlang/imgup.v
+++ vlang/imgup.v
@@ -1,117 +1,170 @@
-import os
-import net.http
-import time
-import encoding.base64
-import json
-
 /*
-Imgup: A small utility to take a screenshot then upload it to imgur.
- - 1. select an area, full screen or current window.
- - 2. upload in imgurl.
- - 3. copy the url into clipboard.
+Imgup: a small utility to take a screenshot then upload it to imgur.
+
+usage:
+- 1. Select mode
+- 2. Upload in imgurl.
+- 3. Copy the url into clipboard.
 
 
 req:
-dmenu, maim, notify-send and xclip
+- Linux: dmenu, maim, notify-send and xclip.
+- Macos: choose-gui, screencapture, pbcopy.
+
 
 TODOs:
-- TODO: fix fullscreen capturing before the dmenu prompt is closed
-- TODO: fix area selection including the mouse.
-- TODO: Add support for gif
-- TODO: Add support for macos
+- [X] Support macos
+- [ ] Fins tool to do send notificationas
+- [ ] Add support for gif
+- [ ] Add support for videos
+- [ ] Add support for videos
+- [ ] fix fullscreen capturing before the dmenu prompt is closed
+- [ ] fix interactive selection including the mouse.
+
+MIT License Copyright (C) tami5 2021-2022
 */
+import os
+import net.http { FetchConfig }
+import time
+import encoding.base64
+import json
 
 struct ImgurRes {
 	status int
 	data   map[string]string
 }
 
-// Send notifcation to the user, TODO: add support for macos
-fn notify_user(msg_title string, msg_content string) {
-	os.system('notify-send "$msg_title" "$msg_content" -t 4000')
-}
-
-// Hacky way of copying a string to system clipboard
-fn clip_str(text string) {
-	os.system('echo $text | xclip -sel clip')
-}
-
-// Return current time and date
+// return current time and date
 fn datetime() string {
 	return time.now().get_fmt_str(.hyphen, .hhmmss24, .ddmmyyyy).replace(' ', '-')
 }
 
-// Select screenshot_mode from a list of `items` through `menu`
-fn choose_mode(menu []string, items map[string]string) string {
-	c := menu.join(' ')
-	o := items.keys().join('\n')
+// Hacky way of copying a string to system clipboard
+fn set_clipboard(text string) ? {
+	clipboard_handler := $if macos { 'pbcopy' } $else { 'xclip -sel clip' }
+	result := os.execute('echo $text | $clipboard_handler')
 
-	r := os.execute('printf "$o" | $c')
-	if r.exit_code != 0 {
-		panic(r.output)
+	if result.exit_code != 0 {
+		return error('failed to copy $text to clipboard: $result.output')
 	}
-	return r.output.trim_space()
+
+	println('imgurl is copied')
 }
 
-// Take a screenshot and return image filepath.
-fn take_screenshot() string {
-	prmpt := 'capture to imgur'
-	dmenu := ['dmenu', '-l', '3', '-i', '-p', '"$prmpt"']
-	maim := {
-		'current':    'maim -i "$(xdotool getactivewindow)"'
-		'area':       'maim -s'
-		'fullscreen': 'maim'
-	}
-	// NOTE: add conditon for current os here
-	opts := maim.clone()
-	menu := dmenu.clone()
-	//
-	mode := choose_mode(menu, opts)
-	if opts[mode].len != 0 {
-		path := '/tmp/${datetime()}.png'
-		cmd := opts[mode]
-		os.system('$cmd $path')
-		return path
-	} else {
-		return ''
+// Send notifcation to the user
+// TODO: add support for macos
+fn notify_user(msg_content string) {
+	$if linux {
+		os.system('notify-send "Imgup" "$msg_content" -t 4000')
 	}
 }
 
-// builder error: Header file <openssl/rand.h>, needed for module `net.openssl` was not found. Please install OpenSSL development headers.
-// Upload image path to imgur
-fn upload_to_imgur(path string) http.Response {
-	file := os.read_file(path) or { panic(err) }
-	req := http.FetchConfig{
+struct UserInterface {
+	picker  []string
+	command string
+	modes   map[string]string
+}
+
+fn get_user_interface() ?UserInterface {
+	prompt := 'Capture to Imgur'
+	$if linux {
+		return UserInterface{
+			picker: ['dmenu', '-l', '3', '-i', '-p', '"$prompt"']
+			command: 'maim'
+			modes: {
+				'current':   '-i "$(xdotool getactivewindow)"'
+				'selection': '-s'
+				'full':      ''
+			}
+		}
+	}
+	$if macos {
+		return UserInterface{
+			picker: ['choose', '-n', '4', '-u']
+			command: 'screencapture'
+			modes: {
+				'window':    '-W'
+				'current':   "-r -l$(yabai -m query --windows --window | jq '.id')"
+				'selection': '-r -i'
+				'full':      '-r'
+			}
+		}
+	} $else {
+		return error('os is not support')
+	}
+}
+
+fn (ui UserInterface) get_mode() ?string {
+	picker := ui.picker.join(' ')
+	choices := ui.modes.keys().join('\n')
+	result := os.execute('printf "$choices" | $picker')
+
+	if result.exit_code != 0 {
+		error(result.output)
+	}
+
+	return result.output.trim_space()
+}
+
+fn (ui UserInterface) capture(mode string) ?string {
+	args := ui.modes[mode]
+	if args == '' {
+		return error('Invalid selection')
+	}
+
+	path := '/tmp/${datetime()}.png'
+	capture := '$ui.command $args $path'
+	result := os.execute(capture)
+
+	if result.exit_code != 0 {
+		return error('"$capture": $result.output')
+	}
+
+	return path
+}
+
+fn upload(path string) ?string {
+	response := http.fetch(FetchConfig{
 		url: 'https://api.imgur.com/3/image'
 		method: .post
+		data: base64.encode(os.read_bytes(path) ?)
 		header: http.new_header_from_map({
 			.authorization: 'Client-ID ea6c0ef2987808e',
 			.content_type:  'image/png',
 			.connection:    'keep-alive',
 		})
-		data: base64.encode(file.bytes())
-	}
-	res := http.fetch(req) or { panic(err) }
-	return res
-}
+	}) ?.text.str()
 
-// Returns imgur url from http.Response
-fn get_img_url(res http.Response) string {
-	r := res.text.str()
-	j := json.decode(ImgurRes, r) or { panic(err) }
-	return j.data['link']
+	return json.decode(ImgurRes, response) ?.data['link']
 }
 
 fn main() {
-	img_path := take_screenshot()
-
-	if img_path.len != 0 {
-		url := get_img_url(upload_to_imgur(img_path))
-		clip_str('"![]($url)"')
-		// clip_str(get_img_url(upload_to_imgur(img_path)))
-		notify_user('IMGUP', 'img link is cliped to clipboard.')
-		exit(0)
+	ui := get_user_interface() or {
+		println('Unable to get user Inteface: $err')
+		exit(1)
 	}
 
-	exit(1)
+	mode := ui.get_mode() or {
+		println('Unable to get mode: $err')
+		exit(1)
+	}
+
+	path := ui.capture(mode) or {
+		println('Fail to capture screen: $err')
+		exit(1)
+	}
+
+	imgurl := upload(path) or {
+		println('Unable to get imgur url: $err')
+		exit(1)
+	}
+
+	set_clipboard('"![]($imgurl)"') or {
+		println(err)
+		exit(1)
+	}
+
+	notify_user('Img link is copied to clipboard.')
+
+	exit(0)
 }
